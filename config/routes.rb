3e11ad59f9fa2fcbfae08268f6551112bc7266b5
johnpmitsch <johnpmitsch@gmail.com>
2017-01-22 07:12:30 -0500

    added extra endpoint for health check

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   get '/_ah/health', to: 'ping#health_check'
+  get '/_ah/vm_health', to: 'ping#health_check'
+  get '/', to: 'ping#health_check'
   get '/ping', to: 'ping#health_check'
   namespace :v1 do
     resources :ingredients
