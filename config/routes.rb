f19654fefc6abdd7f1dba186937746b74a1ef5b0
johnpmitsch <johnpmitsch@gmail.com>
2017-07-24 19:23:07 -0400

    modified registration route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-    registrations:  'registrations'
-  }
+  mount_devise_token_auth_for 'User', at: 'auth'
   get '/', to: 'ping#health_check'
   get '/ping', to: 'ping#health_check'
   namespace :v1 do
